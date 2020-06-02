b430d8df8ebb8b2bf97db0e45467d0c6b8687fc8
Justin Ridge <jridg13@wgu.edu>
2020-06-02 17:18:14 -0400

    Add kmeans script and images

--- api/prep-data.py
+++ api/prep-data.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-popularity = 0
-
 def clean_df(df):
     df = df.drop(['artist_name','track_name','track_id','key','mode','time_signature'],axis=1)
     dummies = pd.get_dummies(df['genre']).rename(columns=lambda x: 'genre_' + str(x))
@@ -20,16 +18,15 @@ def transform_min_max(df):
     data_transformed = pd.DataFrame(data_transformed,columns=df.columns)
     return data_transformed
 
-def is_popular(item):
+def is_popular(item, popularity):
     if item['popularity'] >= popularity:
         return 1
     return 0
 
 def prep_data(df):
     df = clean_df(df)
-    popularity = df['popularity'].quantile(.75)
     labels = pd.DataFrame()
-    labels['target'] = df.apply(lambda row: is_popular(row), axis=1)
+    labels['target'] = df.apply(lambda row: is_popular(row, df['popularity'].mean()), axis=1)
     df = transform_min_max(df)
     df.to_csv('features.csv')
     labels.to_csv('labels.csv')
